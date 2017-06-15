d90f7438eaf8c3339d0d285cb753df28164d9b63
zhishuangyu <zhishuang.yu@mxsoft.com>
2017-06-15 09:38:23 +0800

    新增文件

---
+++ Application/Admin/Controller/GoodsController.class.php
@@ -0,0 +1,30 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+
+class GoodsController extends Controller{
+    public function add(){
+        //2、处理表单
+        if(IS_POST){
+            //3、生成模型
+            $model = D('Goods');
+            if($model->create())
+            {
+                //5、插入数据库
+                if($model->add())
+                {
+                    //6、提示信息
+                    $this->success('操作成功',U('lst'));
+                    //7、停止执行后面的代码
+                    exit;
+                }
+            }
+            //8、如果上面失败，获取失败的原因
+            $error = $model->getError();
+            //9、显示错误信息，并调回到上一个页面
+            $this->error($error);
+        }
+        //1、显示表单
+        $this->display();
+    }
+}
---
+++ Application/Admin/Model/GoodsModel.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace Admin\Model;
+use Think\Model;
+
+class GoodsModel extends Model{
+    //定义表单验证的规则，控制器中的create方法时用
+    protected $_validate = array(
+        array('goods_name','require','商品名称不能为空！',1),
+        array('goods_name','1,45','商品名称必须是1-45个字符',length).
+        array('price','currency','价格必须是货币形式',1),
+        array('is_on_sale','0,1','是否上架只能是0，1两个值',1,'in'),
+    );
+    protected function _before_insert(&$data,$option){
+        //获取当前时间
+        $data['addtime'] = time();
+    }
+}
